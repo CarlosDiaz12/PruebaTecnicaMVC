a683ff5364a81048ec7a087ca576db2dadd76c17
CarlosDiaz12 <carlos.cedp@gmail.com>
2021-08-24 22:26:09 -0400

    Add project files.

---
+++ DependencyInjection/Injection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PruebaTecnica.DataAccess.Interface;
+using PruebaTecnica.DataAccess.Model;
+using PruebaTecnica.DataAccess.Repository;
+
+namespace PruebaTecnicaMVC.DependencyInjection
+{
+    public static class Injection
+    {
+        public static IServiceCollection InjectServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<Estudiante>, EstudianteRepository>();
+            services.AddScoped<IRepository<Profesor>, ProfesorRepository>();
+            services.AddScoped<IRepository<Aula>, AulaRepository>();
+            services.AddScoped<IRepository<Asignatura>, AsignaturaRepository>();
+            services.AddScoped<IRepository<Asignacion>, AsignacionRepository>();
+            //services.AddScoped<EmployeeService, EmployeeService>();
+            return services;
+        }
+
+    }
+}
